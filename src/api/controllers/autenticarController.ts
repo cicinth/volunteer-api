5129a9912b2f66511d6745b1b39ade9a6849cb53
Wellington Nascimento <well.nascimento@outlook.com>
2020-10-21 21:40:13 -0300

    cadatro de usuário do sistema

--- src/api/controllers/autenticarController.ts
+++ src/api/controllers/autenticarController.ts
@@ -1,7 +1,9 @@
-import { AutenticarUsuarioModel } from "../../application/model";
+import {
+  AutenticarUsuarioModel,
+  CadastrarUsuarioModel,
+} from "../../application/model";
 import { IAutenticarApplication } from "../../application";
 import { ApiRegisterType } from "../initApi";
-import * as jwt from "jsonwebtoken";
 
 export default class AutenticarController {
   autenticarApplication: IAutenticarApplication;
@@ -35,6 +37,39 @@ export default class AutenticarController {
       return res.json({ auth: true, token: token });
     } catch (error) {
       return res.error(error);
+    }
+  }
+
+  public async cadastrarAsync(req: any, res: any): Promise<void> {
+    const {
+      celular,
+      cpfCnpj,
+      dtNascimento,
+      email,
+      nome,
+      senha,
+      tipoPessoa,
+    } = req.body;
+
+    const cadastrarUsuarioModel = new CadastrarUsuarioModel();
+    cadastrarUsuarioModel.celular = celular;
+    cadastrarUsuarioModel.cpfCnpj = cpfCnpj;
+    cadastrarUsuarioModel.dtNascimento = dtNascimento;
+    cadastrarUsuarioModel.email = email;
+    cadastrarUsuarioModel.nome = nome;
+    cadastrarUsuarioModel.senha = senha;
+    cadastrarUsuarioModel.tipoPessoa = tipoPessoa;
+
+    try {
+      await this.autenticarApplication.cadastrarNovoUsuarioAsync(
+        cadastrarUsuarioModel
+      );
+      return res.json({
+        status: true,
+        mensagem: "Cadastro efetuado com sucesso",
+      });
+    } catch (error) {
+      return res.error({ status: false, mensagem: error.message, error });
     }
   }
 }
--- src/api/routers/autenticarRouter.ts
+++ src/api/routers/autenticarRouter.ts
@@ -2,5 +2,6 @@ import { IAutenticarApplication } from "../../application";
 import * as express from "express";
 import AutenticarController from "../controllers/autenticarController";
 export default (appExpress:express.Router,autenticarController: AutenticarController)  => {
-  appExpress.get("/autenticar", (req,res)=> autenticarController.autenticarAsync(req,res));
+  appExpress.get("/usuario/autenticar", (req,res)=> autenticarController.autenticarAsync(req,res));
+  appExpress.get("/usuario/cadastrar", (req,res)=> autenticarController.cadastrarAsync(req,res));
 }
